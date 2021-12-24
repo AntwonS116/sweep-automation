c7db2c60fc593ad72fdb7e1779bf55d1fe78b305
Anthony Stevens <astevens@medicom.ca>
2021-12-24 09:01:48 -0500

    updates

--- sweep/scripts.py
+++ sweep/scripts.py
@@ -3,7 +3,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
 
 def login():
 
@@ -22,7 +22,7 @@ def sweep_run():
     frame = driver.find_element_by_tag_name("iframe")
     driver.switch_to.frame(frame)
         
-    for x in range(1, 10):
+    for x in range(0, 5000):
         try:
             time.sleep(1)
 
@@ -50,4 +50,4 @@ def sweep_run():
             next_shipline.click()
 
         except:
-            print("Error")
+            pass    
